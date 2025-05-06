022e09949b0ece09412fed3112db6506e01164c1
Aran12345li12345 <aran32193@gmail.com>
2025-05-06 11:51:48 +0700

    commit database
    masih terkendala pengembangan database untuk user selesaikan dan sisanya inggal di release

---
+++ SKY-Project-main/README.md
@@ -0,0 +1,4 @@
+# SKY-Project
+make sure this project finish 3 month 
+30 July 2025
+launching this website before the deadline 
---
+++ SKY-Project-main/database/-- SQLite.sql
@@ -0,0 +1,6 @@
+-- SQLite
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
